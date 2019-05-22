82d363c4277f293b5cfd7f7871ba37d9613f08ac
sehmbia <sehmbia@sheridancollege.ca>
2019-05-22 09:37:38 -0400

    Initial commit

---
+++ src/studentweek3/Student.java
@@ -0,0 +1,43 @@
+package studentweek3;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author owner
+ */
+public class Student {
+    
+    private int Id;
+    private String firstname;
+
+    /**
+     * @return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    
+    public String getFirstname() {
+        return firstname;
+    }
+
+    
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    
+}
---
+++ src/studentweek3/StudentWeek3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentweek3;
+
+/**
+ *
+ * @author owner
+ */
+public class StudentWeek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student s1 = new Student();
+        s1.setFirstname("john");
+        s1.setId(4);
+    }
+    
+}
